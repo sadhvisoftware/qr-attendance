295987874d0c94e4c53b50e795080cc75f46d9f9
Santhicasting <santhicasting@gmail.com>
2026-03-12 12:52:52 +0530

    debug employee insert

--- db.js
+++ db.js
@@ -1,19 +1,13 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 connection.connect((err)=>{
-  if(err){
-    console.error("DB Connection Failed:",err);
-  }else{
-    console.log("MySQL Connected ✅");
-  }
+if(err){
+console.error("DB Connection Failed:",err);
+}else{
+console.log("MySQL Connected ✅");
+}
 });
 
 module.exports = connection;
